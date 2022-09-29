44bcc7d6d3e3799e66a0ebcf4d9184dc244078c9
pinto0309 <rmsdh122@yahoo.co.jp>
2022-09-29 14:34:42 +0900

    Asin, Asinh, Atan, Atanh

---
+++ onnx2tf/ops/Atan.py
@@ -0,0 +1,42 @@
+import random
+random.seed(0)
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+import onnx_graphsurgeon as gs
+
+
+def make_node(
+    *,
+    graph_node: gs.Node,
+    tf_layers_dict: dict,
+    **kwargs: dict,
+):
+    """Atan
+
+    Parameters
+    ----------
+    graph_node: gs.Node
+        graph_surgeon Node
+
+    tf_layers_dict: dict
+        optype, shape, dtype, tensorflow graph
+    """
+    graph_node_input: gs.Variable = graph_node.inputs[0]
+    graph_node_output: gs.Variable = graph_node.outputs[0]
+    shape = graph_node_output.shape
+    dtype = graph_node_output.dtype
+
+    # Preserving Graph Structure (Dict)
+    tf_layers_dict[graph_node_output.name] = {
+        'optype': graph_node.op,
+        'shape': shape,
+        'dtype': dtype,
+    }
+
+    # Generation of TF OP
+    tf_layers_dict[graph_node_output.name]['tf_node'] = \
+        tf.math.atan(
+            x=tf_layers_dict[graph_node_input.name]['tf_node'],
+            name=graph_node.name,
+        )
