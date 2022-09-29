803ec39c06326df5701e70f048615e2e2f02150f
pinto0309 <rmsdh122@yahoo.co.jp>
2022-09-29 09:28:39 +0900

    And

---
+++ onnx2tf/ops/And.py
@@ -0,0 +1,44 @@
+import random
+from re import A
+random.seed(0)
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+import onnx_graphsurgeon as gs
+
+
+def make_node(
+    *,
+    graph_node: gs.Node,
+    tf_layers_dict: dict,
+):
+    """And
+
+    Parameters
+    ----------
+    graph_node: gs.Node
+        graph_surgeon Node
+
+    tf_layers_dict: dict
+        optype, shape, dtype, tensorflow graph
+    """
+    graph_node_input_1: gs.Variable = graph_node.inputs[0]
+    graph_node_input_2: gs.Variable = graph_node.inputs[1]
+    graph_node_output: gs.Variable = graph_node.outputs[0]
+    shape = graph_node_output.shape
+    dtype = graph_node_output.dtype
+
+    # Preserving Graph Structure (Dict)
+    tf_layers_dict[graph_node_output.name] = {
+        'optype': graph_node.op,
+        'shape': shape,
+        'dtype': dtype,
+    }
+
+    # Generation of TF OP
+    tf_layers_dict[graph_node_output.name]['tf_node'] = \
+        tf.math.logical_and(
+            x=tf_layers_dict[graph_node_input_1.name]['tf_node'],
+            y=tf_layers_dict[graph_node_input_2.name]['tf_node'],
+            name=graph_node.name,
+        )
