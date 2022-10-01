6fdaa253dcae6485b68744b61f88b4c527331fee
pinto0309 <rmsdh122@yahoo.co.jp>
2022-10-01 20:49:15 +0900

    NonZero

---
+++ onnx2tf/ops/NonZero.py
@@ -0,0 +1,52 @@
+import random
+random.seed(0)
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+import onnx_graphsurgeon as gs
+from utils.common_functions import get_constant_or_variable
+
+
+def make_node(
+    *,
+    graph_node: gs.Node,
+    tf_layers_dict: dict,
+    **kwargs: dict,
+):
+    """NonZero
+
+    Parameters
+    ----------
+    graph_node: gs.Node
+        graph_surgeon Node
+
+    tf_layers_dict: dict
+        optype, shape, dtype, tensorflow graph
+    """
+    graph_node_input = get_constant_or_variable(graph_node.inputs[0])
+    graph_node_output: gs.Variable = graph_node.outputs[0]
+
+    shape = graph_node_output.shape
+    dtype = graph_node_output.dtype
+
+    input_tensor = tf_layers_dict[graph_node_input.name]['tf_node'] \
+        if isinstance(graph_node_input, gs.Variable) else graph_node_input
+
+    # Preserving Graph Structure (Dict)
+    tf_layers_dict[graph_node_output.name] = {
+        'optype': graph_node.op,
+        'shape': shape,
+        'dtype': dtype,
+    }
+
+    # Generation of TF OP
+    condition = tf.not_equal(
+        input_tensor,
+        tf.zeros_like(input_tensor),
+    )
+    nonzero_indices = tf.where(condition)
+    tf_layers_dict[graph_node_output.name]['tf_node'] = \
+        tf.transpose(
+            a=nonzero_indices,
+            name=graph_node.name,
+        )
