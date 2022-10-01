6c7d4e447ee52051cd92c586b27c998a3dc317e9
pinto0309 <rmsdh122@yahoo.co.jp>
2022-10-01 16:09:54 +0900

    ReduceMax, Mean, Min, Prod

---
+++ onnx2tf/ops/ReduceMax.py
@@ -0,0 +1,64 @@
+import random
+random.seed(0)
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+import onnx_graphsurgeon as gs
+from utils.common_functions import (
+    convert_axis,
+    get_constant_or_variable,
+)
+
+
+def make_node(
+    *,
+    graph_node: gs.Node,
+    tf_layers_dict: dict,
+    **kwargs: dict,
+):
+    """ReduceMax
+
+    Parameters
+    ----------
+    graph_node: gs.Node
+        graph_surgeon Node
+
+    tf_layers_dict: dict
+        optype, shape, dtype, tensorflow graph
+    """
+    graph_node_input = get_constant_or_variable(graph_node.inputs[0])
+    graph_node_output: gs.Variable = graph_node.outputs[0]
+    shape = graph_node_output.shape
+    dtype = graph_node_output.dtype
+
+    tensor_rank = len(graph_node_input.shape)
+
+    axes = graph_node.attrs.get('axes', [-1])
+    # NCHW->NHWC, NCDHW->NDHWC
+    axes = convert_axis(
+        axis=axes,
+        tensor_rank=tensor_rank,
+    )
+    axes = [convert_axis(axis=idx, tensor_rank=tensor_rank) for idx in axes]
+
+    # 0: False, 1: True
+    keepdims = bool(graph_node.attrs.get('keepdims', 1))
+
+    # Preserving Graph Structure (Dict)
+    tf_layers_dict[graph_node_output.name] = {
+        'optype': graph_node.op,
+        'shape': shape,
+        'dtype': dtype,
+    }
+
+    # Generation of TF OP
+    reducemaxed_tensor = tf_layers_dict[graph_node_input.name]['tf_node'] \
+        if isinstance(graph_node_input, gs.Variable) else graph_node_input
+    for idx in axes:
+        reducemaxed_tensor = tf.math.reduce_max(
+            input_tensor=reducemaxed_tensor,
+            axis=idx,
+            keepdims=keepdims,
+            name=f'{graph_node.name}_{idx}',
+        )
+    tf_layers_dict[graph_node_output.name]['tf_node'] = reducemaxed_tensor
