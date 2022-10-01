6f51b96999e6a25bfcf446a8201af6aa574466d1
pinto0309 <rmsdh122@yahoo.co.jp>
2022-10-01 21:39:19 +0900

    ReduceLogSumExp

---
+++ onnx2tf/ops/ReduceLogSumExp.py
@@ -0,0 +1,59 @@
+import random
+random.seed(0)
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+import onnx_graphsurgeon as gs
+from utils.common_functions import (
+    get_constant_or_variable,
+    convert_axis,
+)
+
+
+def make_node(
+    *,
+    graph_node: gs.Node,
+    tf_layers_dict: dict,
+    **kwargs: dict,
+):
+    """ReduceLogSumExp
+
+    Parameters
+    ----------
+    graph_node: gs.Node
+        graph_surgeon Node
+
+    tf_layers_dict: dict
+        optype, shape, dtype, tensorflow graph
+    """
+    graph_node_input = get_constant_or_variable(graph_node.inputs[0])
+    graph_node_output: gs.Variable = graph_node.outputs[0]
+    shape = graph_node_output.shape
+    dtype = graph_node_output.dtype
+
+    input_tensor = tf_layers_dict[graph_node_input.name]['tf_node'] \
+        if isinstance(graph_node_input, gs.Variable) else graph_node_input
+    tensor_rank = len(input_tensor.shape)
+
+    axes = graph_node.attrs.get('axes', [])
+    if isinstance(axes, list) or (isinstance(axes, np.ndarray) and len(axes.shape) > 0):
+        axes = [convert_axis(axis=idx, tensor_rank=tensor_rank) for idx in axes]
+    elif axes is not None and isinstance(axes, np.ndarray) and len(axes.shape) == 0:
+        axes = convert_axis(axis=axes, tensor_rank=tensor_rank)
+    keepdims = bool(graph_node.attrs.get('keepdims', 1))
+
+    # Preserving Graph Structure (Dict)
+    tf_layers_dict[graph_node_output.name] = {
+        'optype': graph_node.op,
+        'shape': shape,
+        'dtype': dtype,
+    }
+
+    # Generation of TF OP
+    tf_layers_dict[graph_node_output.name]['tf_node'] = \
+        tf.math.reduce_logsumexp(
+            input_tensor=input_tensor,
+            axis=list(axes) if axes is not None else None,
+            keepdims=keepdims,
+            name=graph_node.name,
+        )
