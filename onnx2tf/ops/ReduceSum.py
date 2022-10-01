e68e877a7d99de58e1069894d0ddae7b6dfc3d36
pinto0309 <rmsdh122@yahoo.co.jp>
2022-10-01 16:36:12 +0900

    ReduceSum

---
+++ onnx2tf/ops/ReduceSum.py
@@ -0,0 +1,77 @@
+import random
+random.seed(0)
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+import onnx_graphsurgeon as gs
+from utils.common_functions import (
+    get_constant_or_variable,
+    convert_axis,
+)
+from utils.colors import Color
+
+
+def make_node(
+    *,
+    graph_node: gs.Node,
+    tf_layers_dict: dict,
+    **kwargs: dict,
+):
+    """ReduceSum
+
+    Parameters
+    ----------
+    graph_node: gs.Node
+        graph_surgeon Node
+
+    tf_layers_dict: dict
+        optype, shape, dtype, tensorflow graph
+    """
+    graph_node_input_1 = get_constant_or_variable(graph_node.inputs[0])
+    graph_node_input_2 = None
+    if len(graph_node.inputs) == 2:
+        graph_node_input_2 = get_constant_or_variable(graph_node.inputs[1])
+    graph_node_output: gs.Variable = graph_node.outputs[0]
+    shape = graph_node_output.shape
+    dtype = graph_node_output.dtype
+
+    input_tensor = tf_layers_dict[graph_node_input_1.name]['tf_node'] \
+        if isinstance(graph_node_input_1, gs.Variable) else graph_node_input_1
+    tensor_rank = len(input_tensor.shape)
+
+    axes = tf_layers_dict[graph_node_input_2.name]['tf_node'] \
+        if isinstance(graph_node_input_2, gs.Variable) else graph_node_input_2
+    if axes is not None and axes.shape is None:
+        axes = None
+
+    axes = graph_node.attrs.get('axes', axes)
+    noop_with_empty_axes = bool(graph_node.attrs.get('noop_with_empty_axes', 0))
+    if noop_with_empty_axes:
+        error_msg = f'' +\
+            f'{Color.RED}ERROR:{Color.RESET} ' +\
+            f'TensorFlow does not support noop_with_empty_axes=1 (True).'
+        print(error_msg)
+        assert not noop_with_empty_axes, error_msg
+
+    if isinstance(axes, list) or (isinstance(axes, np.ndarray) and len(axes.shape) > 0):
+        axes = [convert_axis(axis=idx, tensor_rank=tensor_rank) for idx in axes]
+    elif axes is not None and isinstance(axes, np.ndarray) and len(axes.shape) == 0:
+        axes = convert_axis(axis=axes, tensor_rank=tensor_rank)
+
+    # Preserving Graph Structure (Dict)
+    tf_layers_dict[graph_node_output.name] = {
+        'optype': graph_node.op,
+        'shape': shape,
+        'dtype': dtype,
+    }
+
+    # Generation of TF OP
+    reducesumed_tensor = input_tensor
+    for idx in axes:
+        reducesumed_tensor = \
+            tf.reduce_sum(
+                input_tensor=reducesumed_tensor,
+                axis=idx,
+                name=f'{graph_node.name}_{idx}',
+            )
+    tf_layers_dict[graph_node_output.name]['tf_node'] = reducesumed_tensor
