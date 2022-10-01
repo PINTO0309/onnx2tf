818de6a9b036c3303d59ca0003b744aaa81e98c5
pinto0309 <rmsdh122@yahoo.co.jp>
2022-10-01 21:08:50 +0900

    RandomNormal, RandomNormalLike, RandomUniform, RandomUniformLike

---
+++ onnx2tf/ops/RandomUniform.py
@@ -0,0 +1,53 @@
+import random
+random.seed(0)
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+import onnx_graphsurgeon as gs
+from utils.enums import ONNX_DTYPES_TO_TF_DTYPES
+
+
+def make_node(
+    *,
+    graph_node: gs.Node,
+    tf_layers_dict: dict,
+    **kwargs: dict,
+):
+    """RandomUniform
+
+    Parameters
+    ----------
+    graph_node: gs.Node
+        graph_surgeon Node
+
+    tf_layers_dict: dict
+        optype, shape, dtype, tensorflow graph
+    """
+    graph_node_output: gs.Variable = graph_node.outputs[0]
+
+    shape = graph_node_output.shape
+    dtype = graph_node_output.dtype
+
+    rdtype = graph_node.attrs.get('dtype', 1)
+    rhigh = graph_node.attrs.get('high', 1.0)
+    rlow = graph_node.attrs.get('low', 0.0)
+    rseed = graph_node.attrs.get('seed', 0)
+    rshape = graph_node.attrs['shape']
+
+    # Preserving Graph Structure (Dict)
+    tf_layers_dict[graph_node_output.name] = {
+        'optype': graph_node.op,
+        'shape': shape,
+        'dtype': dtype,
+    }
+
+    # Generation of TF OP
+    tf_layers_dict[graph_node_output.name]['tf_node'] = \
+        tf.random.uniform(
+            shape=rshape,
+            minval=rlow,
+            maxval=rhigh,
+            dtype=ONNX_DTYPES_TO_TF_DTYPES[rdtype],
+            seed=rseed,
+            name=graph_node.name,
+        )
