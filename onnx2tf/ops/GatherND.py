2c97d801677a5a2518c1e31451210cb8119c9f57
pinto0309 <rmsdh122@yahoo.co.jp>
2022-10-01 17:15:01 +0900

    GatherND

---
+++ onnx2tf/ops/GatherND.py
@@ -0,0 +1,58 @@
+import random
+random.seed(0)
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+import onnx_graphsurgeon as gs
+from utils.common_functions import (
+    get_constant_or_variable,
+    convert_axis,
+)
+
+
+def make_node(
+    *,
+    graph_node: gs.Node,
+    tf_layers_dict: dict,
+    **kwargs: dict,
+):
+    """GatherND
+
+    Parameters
+    ----------
+    graph_node: gs.Node
+        graph_surgeon Node
+
+    tf_layers_dict: dict
+        optype, shape, dtype, tensorflow graph
+    """
+    graph_node_input_1 = get_constant_or_variable(graph_node.inputs[0])
+    graph_node_input_2 = get_constant_or_variable(graph_node.inputs[1])
+    graph_node_output: gs.Variable = graph_node.outputs[0]
+    shape = graph_node_output.shape
+    dtype = graph_node_output.dtype
+
+    input_tensor = tf_layers_dict[graph_node_input_1.name]['tf_node'] \
+        if isinstance(graph_node_input_1, gs.Variable) else graph_node_input_1
+    indices_tensor = tf_layers_dict[graph_node_input_2.name]['tf_node'] \
+        if isinstance(graph_node_input_2, gs.Variable) else graph_node_input_2
+    tensor_rank = len(input_tensor.shape)
+
+    batch_dims = graph_node.attrs.get('batch_dims', 0)
+    batch_dims = convert_axis(axis=batch_dims, tensor_rank=tensor_rank)
+
+    # Preserving Graph Structure (Dict)
+    tf_layers_dict[graph_node_output.name] = {
+        'optype': graph_node.op,
+        'shape': shape,
+        'dtype': dtype,
+    }
+
+    # Generation of TF OP
+    tf_layers_dict[graph_node_output.name]['tf_node'] = \
+        tf.gather_nd(
+            params=input_tensor,
+            indices=indices_tensor,
+            batch_dims=batch_dims,
+            name=graph_node.name,
+        )
