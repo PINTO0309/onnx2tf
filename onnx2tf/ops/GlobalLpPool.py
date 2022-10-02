47f3148ceeec1f393f4392b18997e8043918d6dc
pinto0309 <rmsdh122@yahoo.co.jp>
2022-10-03 00:31:15 +0900

    Globalxxx

---
+++ onnx2tf/ops/GlobalLpPool.py
@@ -0,0 +1,57 @@
+import random
+random.seed(0)
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+import onnx_graphsurgeon as gs
+from utils.common_functions import get_constant_or_variable
+
+
+def make_node(
+    *,
+    graph_node: gs.Node,
+    tf_layers_dict: dict,
+    **kwargs: dict,
+):
+    """GlobalLpPool
+
+    Parameters
+    ----------
+    graph_node: gs.Node
+        graph_surgeon Node
+
+    tf_layers_dict: dict
+        optype, shape, dtype, tensorflow graph
+    """
+    graph_node_input = get_constant_or_variable(graph_node.inputs[0])
+    graph_node_output: gs.Variable = graph_node.outputs[0]
+
+    input_tensor = tf_layers_dict[graph_node_input.name]['tf_node'] \
+        if isinstance(graph_node_input, gs.Variable) else graph_node_input
+    input_tensor_rank = len(input_tensor.shape)
+
+    p = graph_node.attrs.get('p', 2)
+
+    shape = graph_node_output.shape
+    dtype = graph_node_output.dtype
+
+    # Preserving Graph Structure (Dict)
+    tf_layers_dict[graph_node_output.name] = {
+        'optype': graph_node.op,
+        'shape': shape,
+        'dtype': dtype,
+    }
+
+    # Generation of TF OP
+    dims = list(range(input_tensor_rank))
+    dim_window = dims[1:-1]
+    if len(dim_window) > 1 and p == 2:
+        p = "euclidean"
+    tf_layers_dict[graph_node_output.name]['tf_node'] = \
+        tf.norm(
+            input_tensor,
+            ord=p,
+            axis=dim_window,
+            keepdims=True,
+        )
+
---
+++ onnx2tf/ops/GlobalMaxPool.py
@@ -0,0 +1,51 @@
+import random
+random.seed(0)
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+import onnx_graphsurgeon as gs
+from utils.common_functions import get_constant_or_variable
+
+
+def make_node(
+    *,
+    graph_node: gs.Node,
+    tf_layers_dict: dict,
+    **kwargs: dict,
+):
+    """GlobalMaxPool
+
+    Parameters
+    ----------
+    graph_node: gs.Node
+        graph_surgeon Node
+
+    tf_layers_dict: dict
+        optype, shape, dtype, tensorflow graph
+    """
+    graph_node_input = get_constant_or_variable(graph_node.inputs[0])
+    graph_node_output: gs.Variable = graph_node.outputs[0]
+
+    input_tensor = tf_layers_dict[graph_node_input.name]['tf_node'] \
+        if isinstance(graph_node_input, gs.Variable) else graph_node_input
+    input_tensor_rank = len(input_tensor.shape)
+
+    shape = graph_node_output.shape
+    dtype = graph_node_output.dtype
+
+    # Preserving Graph Structure (Dict)
+    tf_layers_dict[graph_node_output.name] = {
+        'optype': graph_node.op,
+        'shape': shape,
+        'dtype': dtype,
+    }
+
+    # Generation of TF OP
+    axis = [dim for dim in range(1, input_tensor_rank - 1)]
+    tf_layers_dict[graph_node_output.name]['tf_node'] = \
+        tf.reduce_max(
+            input_tensor=input_tensor,
+            axis=axis,
+            keepdims=True,
+            name=graph_node.name,
+        )
