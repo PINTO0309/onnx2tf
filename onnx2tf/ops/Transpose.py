5178c4bc9c5c6004b49417706366b202adae9aa5
pinto0309 <rmsdh122@yahoo.co.jp>
2022-10-04 17:43:28 +0900

    bugfixes

--- onnx2tf/ops/Transpose.py
+++ onnx2tf/ops/Transpose.py
@@ -7,7 +7,6 @@ import onnx_graphsurgeon as gs
 from onnx2tf.utils.common_functions import (
     get_constant_or_variable,
     convert_axis,
-    convert_reverse_axis,
     print_node_info,
     inverted_operation_enable_disable,
 )
