cc1dc0bb5ecdd15f1d0da5b91f6728be049e3fbb
pinto0309 <rmsdh122@yahoo.co.jp>
2022-10-05 17:36:40 +0900

    Sum

---
+++ onnx2tf/ops/Sum.py
@@ -0,0 +1,67 @@
+import random
+random.seed(0)
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+import onnx_graphsurgeon as gs
+from onnx2tf.utils.common_functions import (
+    get_constant_or_variable,
+    print_node_info,
+    inverted_operation_enable_disable,
+)
+
+
+@print_node_info
+@inverted_operation_enable_disable
+def make_node(
+    *,
+    graph_node: gs.Node,
+    tf_layers_dict: dict,
+    **kwargs: dict,
+):
+    """Sum
+
+    Parameters
+    ----------
+    graph_node: gs.Node
+        graph_surgeon Node
+
+    tf_layers_dict: dict
+        optype, shape, dtype, tensorflow graph
+    """
+    before_op_output_shape_trans = True
+    for graph_node_input in graph_node.inputs:
+        before_op_output_shape_trans_n = \
+            tf_layers_dict.get(graph_node_input.name, {}).get('before_op_output_shape_trans', True)
+        before_op_output_shape_trans = \
+            before_op_output_shape_trans and before_op_output_shape_trans_n
+
+    values = []
+    for graph_node_input in graph_node.inputs:
+        const_or_var = get_constant_or_variable(
+            graph_node_input,
+            before_op_output_shape_trans,
+        )
+        if isinstance(const_or_var, gs.Variable):
+            values.append(tf_layers_dict[const_or_var.name]['tf_node'])
+        else:
+            values.append(const_or_var)
+
+    graph_node_output: gs.Variable = graph_node.outputs[0]
+
+    shape = graph_node_output.shape
+    dtype = graph_node_output.dtype
+
+    # Preserving Graph Structure (Dict)
+    tf_layers_dict[graph_node_output.name] = {
+        'optype': graph_node.op,
+        'shape': shape,
+        'dtype': dtype,
+    }
+
+    # Generation of TF OP
+    tf_layers_dict[graph_node_output.name]['tf_node'] = \
+        tf.add_n(
+            input_tensor=values,
+            name=graph_node.name,
+        )
