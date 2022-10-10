06a2e62c15a56c9ca2f1e76624aaa6c87c603ab4
pinto0309 <rmsdh122@yahoo.co.jp>
2022-10-11 01:17:37 +0900

    MeanVarianceNormalization

---
+++ onnx2tf/ops/MeanVarianceNormalization.py
@@ -0,0 +1,84 @@
+import random
+random.seed(0)
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+import onnx_graphsurgeon as gs
+from onnx2tf.utils.common_functions import (
+    get_constant_or_variable,
+    print_node_info,
+    inverted_operation_enable_disable,
+    convert_axis,
+)
+
+
+@print_node_info
+@inverted_operation_enable_disable
+def make_node(
+    *,
+    graph_node: gs.Node,
+    tf_layers_dict: dict,
+    **kwargs: dict,
+):
+    """MeanVarianceNormalization
+
+    Parameters
+    ----------
+    graph_node: gs.Node
+        graph_surgeon Node
+
+    tf_layers_dict: dict
+        optype, shape, dtype, tensorflow graph
+    """
+    before_op_output_shape_trans_1 = \
+        tf_layers_dict.get(graph_node.inputs[0].name, {}).get('before_op_output_shape_trans', True)
+    before_op_output_shape_trans = \
+        before_op_output_shape_trans_1
+
+    graph_node_input = get_constant_or_variable(
+        graph_node.inputs[0],
+        before_op_output_shape_trans,
+    )
+    input_tensor = tf_layers_dict[graph_node_input.name]['tf_node'] \
+        if isinstance(graph_node_input, gs.Variable) else graph_node_input
+    input_tensor_rank = len(input_tensor.shape)
+
+
+    graph_node_output: gs.Variable = graph_node.outputs[0]
+
+    shape = graph_node_output.shape
+    dtype = graph_node_output.dtype
+
+    axes = graph_node.attrs.get('axes', [0,2,3])
+    if isinstance(axes, list) or (isinstance(axes, np.ndarray) and len(axes.shape) > 0):
+        axes = [
+            convert_axis(
+                axis=idx,
+                tensor_rank=input_tensor_rank,
+                before_op_output_shape_trans=before_op_output_shape_trans,
+            ) for idx in axes
+        ]
+    elif axes is not None and isinstance(axes, np.ndarray) and len(axes.shape) == 0:
+        axes = convert_axis(
+            axis=axes,
+            tensor_rank=input_tensor_rank,
+            before_op_output_shape_trans=before_op_output_shape_trans,
+        )
+
+
+    # Preserving Graph Structure (Dict)
+    tf_layers_dict[graph_node_output.name] = {
+        'optype': graph_node.op,
+        'shape': shape,
+        'dtype': dtype,
+    }
+
+    # Generation of TF OP
+    moments_axes = [0] if input_tensor_rank < 3 else axes
+    mean, variance = tf.nn.moments(
+        x=input_tensor,
+        axes=moments_axes,
+        keepdims=True,
+    )
+    tf_layers_dict[graph_node_output.name]['tf_node'] = \
+        (input_tensor - mean) / tf.sqrt(variance)
