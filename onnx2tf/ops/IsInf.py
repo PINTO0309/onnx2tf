9f1a272732168c19f70fb7623957d214bd22e131
pinto0309 <rmsdh122@yahoo.co.jp>
2022-10-12 11:01:16 +0900

    IsInf, IsNaN, LRN

---
+++ onnx2tf/ops/IsInf.py
@@ -0,0 +1,87 @@
+import random
+random.seed(0)
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+import onnx_graphsurgeon as gs
+from onnx2tf.utils.common_functions import (
+    get_constant_or_variable,
+    print_node_info,
+    inverted_operation_enable_disable,
+    make_tf_node_info,
+)
+
+
+@print_node_info
+@inverted_operation_enable_disable
+def make_node(
+    *,
+    graph_node: gs.Node,
+    tf_layers_dict: dict,
+    **kwargs: dict,
+):
+    """IsInf
+
+    Parameters
+    ----------
+    graph_node: gs.Node
+        graph_surgeon Node
+
+    tf_layers_dict: dict
+        optype, shape, dtype, tensorflow graph
+    """
+    before_op_output_shape_trans_1 = \
+        tf_layers_dict.get(graph_node.inputs[0].name, {}).get('before_op_output_shape_trans', True)
+    before_op_output_shape_trans = \
+        before_op_output_shape_trans_1
+
+    graph_node_input_1 = get_constant_or_variable(
+        graph_node.inputs[0],
+        before_op_output_shape_trans,
+    )
+
+    graph_node_output: gs.Variable = graph_node.outputs[0]
+    shape = graph_node_output.shape
+    dtype = graph_node_output.dtype
+
+    # Preserving Graph Structure (Dict)
+    tf_layers_dict[graph_node_output.name] = {
+        'optype': graph_node.op,
+        'shape': shape,
+        'dtype': dtype,
+    }
+
+    # Generation of TF OP
+    input_tensor_1 = tf_layers_dict[graph_node_input_1.name]['tf_node'] \
+        if isinstance(graph_node_input_1, gs.Variable) else graph_node_input_1
+
+    zero = tf.zeros(shape, dtype)
+    dn = bool(graph_node.attrs.get('detect_negative', 1))
+    dp = bool(graph_node.attrs.get('detect_positive', 1))
+    inp = input_tensor_1
+    if dn == 0:
+        inp = tf.maximum(zero, inp)
+    if dp == 0:
+        inp = tf.minimum(zero, inp)
+
+    tf_layers_dict[graph_node_output.name]['tf_node'] = \
+        tf.math.is_inf(
+            x=inp,
+            name=graph_node.name,
+        )
+
+    # Generation of Debug Info
+    tf_layers_dict[graph_node_output.name]['tf_node_info'] = \
+        make_tf_node_info(
+            node_info={
+                'tf_op_type': tf.math.is_inf,
+                'tf_inputs': {
+                    'x': input_tensor_1,
+                    'detect_negative': dn,
+                    'detect_positive': dp,
+                },
+                'tf_outputs': {
+                    'output': tf_layers_dict[graph_node_output.name]['tf_node'],
+                },
+            }
+        )
