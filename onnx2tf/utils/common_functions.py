775ef22fccd67551b5ceb01e5b3b8f7cf445981b
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-03 00:42:35 +0900

    Functionalize automatic transposition of input values

--- onnx2tf/utils/common_functions.py
+++ onnx2tf/utils/common_functions.py
@@ -1943,3 +1943,55 @@ def disable_unnecessary_transpose(
                         perm=reverse_perm,
                     )
     return graph_node_input_1, graph_node_input_2, input_tensor_1, input_tensor_2
+
+
+def shape_unmatched_special_avoidance_workaround(
+    *,
+    graph_node_input_1,
+    graph_node_input_2,
+    input_tensor_1,
+    input_tensor_2,
+    tf_layers_dict,
+):
+    # At least one True value for same_input_shape_as_onnx
+    # At least one True value in nhwc_flags
+    # same_input_shape_as_onnx == True and nhwc_flags == False and 3D or 4D or 5D tensor is NHWC transposed
+    nhwc_flag_1 = False
+    same_input_shape_as_onnx_1 = False
+    if isinstance(input_tensor_1, gs.Variable):
+        nhwc_flag_1 =tf_layers_dict[input_tensor_1.name]['nhwc'] \
+            if 'nhwc' in tf_layers_dict[input_tensor_1.name].keys() else False
+        same_input_shape_as_onnx_1 = True if len(graph_node_input_1.shape) > 0 \
+            and graph_node_input_1.shape == tf_layers_dict[input_tensor_1.name]['tf_node'].shape else False
+    else:
+        nhwc_flag_1 = False
+        same_input_shape_as_onnx_1 = True if len(graph_node_input_1.shape) > 0 \
+            and graph_node_input_1.shape == input_tensor_1.shape else False
+    nhwc_flag_2 = False
+    same_input_shape_as_onnx_2 = False
+    if isinstance(input_tensor_2, gs.Variable):
+        nhwc_flag_2 =tf_layers_dict[input_tensor_2.name]['nhwc'] \
+            if 'nhwc' in tf_layers_dict[input_tensor_2.name].keys() else False
+        same_input_shape_as_onnx_2 = True if len(graph_node_input_2.shape) > 0 \
+            and graph_node_input_2.shape == tf_layers_dict[input_tensor_2.name]['tf_node'].shape else False
+    else:
+        nhwc_flag_2 = False
+        same_input_shape_as_onnx_2 = True if len(graph_node_input_2.shape) > 0 \
+            and graph_node_input_2.shape == input_tensor_2.shape else False
+
+    same_input_shape_as_onnxs = [same_input_shape_as_onnx_1, same_input_shape_as_onnx_2]
+    nhwc_flags = [nhwc_flag_1, nhwc_flag_2]
+    if True in same_input_shape_as_onnxs and True in nhwc_flags:
+        values = [input_tensor_1, input_tensor_2]
+        for idx, (same_input_shape_as_onnx, nhwc_flag) in enumerate(zip(same_input_shape_as_onnxs, nhwc_flags)):
+            if same_input_shape_as_onnx and not nhwc_flag:
+                if len(values[idx].shape) == 3:
+                    values[idx] = tf.transpose(a=values[idx], perm=[0,2,1])
+                elif len(values[idx].shape) == 4:
+                    values[idx] = tf.transpose(a=values[idx], perm=[0,2,3,1])
+                elif len(values[idx].shape) == 5:
+                    values[idx] = tf.transpose(a=values[idx], perm=[0,2,3,4,1])
+    input_tensor_1 = values[0]
+    input_tensor_2 = values[1]
+
+    return input_tensor_1, input_tensor_2
