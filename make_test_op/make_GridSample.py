2100d9cae4309cdcbbec9b63d2ab0609277fa7c1
pinto0309 <rmsdh122@yahoo.co.jp>
2023-02-22 14:21:28 +0900

    GridSample

--- make_test_op/make_GridSample.py
+++ make_test_op/make_GridSample.py
@@ -1,100 +1,104 @@
-import tensorflow as tf
+import torch
+import torch.nn as nn
 
-def bilinear_sample_noloop(image, grid):
-    """
-    image
-        [N, H, W, C]
-    grid
-        [N, grid_H, grid_W, 2]
-    """
-    Nt, H, W, C = image.shape
-    grid_H = grid.shape[1]
-    grid_W = grid.shape[2]
-    xgrid, ygrid = tf.split(
-        value=grid,
-        num_or_size_splits=2,
-        axis=-1,
-    )
-    mask = tf.cast(
-        (xgrid >= 0) & (ygrid >= 0) & (xgrid < W - 1) & (ygrid < H - 1),
-        dtype=tf.float32,
-    )
-    x0 = tf.math.floor(xgrid)
-    x1 = x0 + 1
-    y0 = tf.math.floor(ygrid)
-    y1 = y0 + 1
 
-    wa = tf.transpose(
-        a=(x1 - xgrid) * (y1 - ygrid),
-        perm=[3, 0, 1, 2],
-    )
-    wb = tf.transpose(
-        a=(x1 - xgrid) * (ygrid - y0),
-        perm=[3, 0, 1, 2],
-    )
-    wc = tf.transpose(
-        a=(xgrid - x0) * (y1 - ygrid),
-        perm=[3, 0, 1, 2],
-    )
-    wd = tf.transpose(
-        a=(xgrid - x0) * (ygrid - y0),
-        perm=[3, 0, 1, 2],
-    )
+class Model16(nn.Module):
+    def __init__(self):
+        super(Model16, self).__init__()
 
-    x0 = tf.cast(
-        tf.reshape(
-            tensor=(x0 * mask),
-            shape=[Nt, grid_H, grid_W],
-        ),
-        dtype=tf.int64,
-    )
-    y0 = tf.cast(
-        tf.reshape(
-            tensor=(y0 * mask),
-            shape=[Nt, grid_H, grid_W]
-        ),
-        dtype=tf.int64,
-    )
-    x1 = tf.cast(
-        tf.reshape(
-            tensor=(x1 * mask),
-            shape=[Nt, grid_H, grid_W]
-        ),
-        dtype=tf.int64,
-    )
-    y1 = tf.cast(
-        tf.reshape(
-            tensor=(y1 * mask),
-            shape=[Nt, grid_H, grid_W]
-        ),
-        dtype=tf.int64,
-    )
+    def forward(self, x, grid):
+        ret = torch.nn.functional.grid_sample(input=x, grid=grid)
+        return ret
 
-    ind = tf.range(limit=Nt)
-    ind = tf.reshape(tensor=ind, shape=[Nt, 1])
-    ind = tf.tile(input=ind, multiples=[1, grid_H])
-    ind = tf.reshape(tensor=ind, shape=[Nt, grid_H, 1])
-    ind = tf.tile(input=ind, multiples=[1, 1, grid_W])
-    ind = tf.cast(ind, dtype=tf.int64)
+class Model11(nn.Module):
+    def __init__(self):
+        super(Model11, self).__init__()
 
-    image = tf.transpose(
-        a=image,
-        perm=[3,0,1,2],
-    )
-    output_tensor = \
-        image[:, ind, y0, x0] * wa \
-        + image[:, ind, y1, x0] * wb \
-        + image[:, ind, y0, x1] * wc \
-        + image[:, ind, y1, x1] * wd
-    output_tensor = tf.transpose(
-        a=output_tensor,
-        perm=[1,2,3,0],
-    )
-    mask = tf.tile(
-        input=mask,
-        multiples=[1,1,1,C],
-    )
-    output_tensor = output_tensor * mask
+    def forward(self, x, grid):
+        Nt, C, H, W = x.shape
+        grid_H = grid.shape[1]
+        grid_W = grid.shape[2]
+        xgrid, ygrid = torch.split(
+            tensor=grid,
+            split_size_or_sections=[1, 1],
+            dim=-1,
+        )
+        mask = (
+            (xgrid >= 0) & (ygrid >= 0) & (xgrid < W - 1) & (ygrid < H - 1)
+        ).float()
+        x0 = torch.floor(xgrid)
+        x1 = x0 + 1
+        y0 = torch.floor(ygrid)
+        y1 = y0 + 1
+        wa = ((x1 - xgrid) * (y1 - ygrid)).permute(3, 0, 1, 2)
+        wb = ((x1 - xgrid) * (ygrid - y0)).permute(3, 0, 1, 2)
+        wc = ((xgrid - x0) * (y1 - ygrid)).permute(3, 0, 1, 2)
+        wd = ((xgrid - x0) * (ygrid - y0)).permute(3, 0, 1, 2)
+        x0 = (x0 * mask).view(Nt, grid_H, grid_W).long()
+        y0 = (y0 * mask).view(Nt, grid_H, grid_W).long()
+        x1 = (x1 * mask).view(Nt, grid_H, grid_W).long()
+        y1 = (y1 * mask).view(Nt, grid_H, grid_W).long()
+        ind = torch.arange(Nt)
+        ind = ind\
+            .view(Nt, 1)\
+            .expand(-1, grid_H)\
+            .view(Nt, grid_H, 1)\
+            .expand(-1, -1, grid_W)\
+            .long()
+        x = x.permute(1, 0, 2, 3)
+        output_tensor = (
+            x[:, ind, y0, x0] * wa \
+            + x[:, ind, y1, x0] * wb \
+            + x[:, ind, y0, x1] * wc \
+            + x[:, ind, y1, x1] * wd
+        ).permute(1, 0, 2, 3)
+        ret = output_tensor * mask.permute(0, 3, 1, 2).expand(-1, C, -1, -1)
+        return ret
 
-    return output_tensor
 
+model = Model16()
+x = torch.randn([1,3,224,224])
+grid = torch.randn([1,32,32,2])
+onnx_file = f'GridSample_16.onnx'
+torch.onnx.export(
+    model,
+    args=(x, grid),
+    f=onnx_file,
+    opset_version=16,
+    input_names=[
+        'image',
+        'grid',
+    ],
+    output_names=[
+        'output',
+    ],
+)
+import onnx
+from onnxsim import simplify
+model_onnx2 = onnx.load(onnx_file)
+model_simp, check = simplify(model_onnx2)
+onnx.save(model_simp, onnx_file)
+
+
+model = Model11()
+x = torch.randn([1,3,224,224])
+grid = torch.randn([1,32,32,2])
+onnx_file = f'GridSample_11.onnx'
+torch.onnx.export(
+    model,
+    args=(x, grid),
+    f=onnx_file,
+    opset_version=11,
+    input_names=[
+        'image',
+        'grid',
+    ],
+    output_names=[
+        'output',
+    ],
+)
+import onnx
+from onnxsim import simplify
+model_onnx2 = onnx.load(onnx_file)
+model_simp, check = simplify(model_onnx2)
+onnx.save(model_simp, onnx_file)
