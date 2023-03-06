468ae3f41597349ecb1c2b56d32408c69a08af37
pinto0309 <rmsdh122@yahoo.co.jp>
2023-03-06 12:59:32 +0900

    Merge two consecutive identical OPs into one

--- onnx2tf/ops/Div.py
+++ onnx2tf/ops/Div.py
@@ -19,6 +19,7 @@ from onnx2tf.utils.common_functions import (
     shape_unmatched_special_avoidance_workaround,
     make_tf_partial_model_inputs,
     dummy_tf_inference,
+    merge_two_consecutive_identical_ops_into_one,
 )
 from typing import Any, Dict, List
 from onnx2tf.utils.enums import TF_DTYPES_TO_NUMPY_DTYPES
@@ -192,15 +193,34 @@ def make_node(
 
     # Generation of TF OP
     ### Overall model
-    divided_tensor = tf.math.divide(
-        x=input_tensor_1 \
-            if not isinstance(input_tensor_1, np.ndarray) \
-                else tf.convert_to_tensor(input_tensor_1),
-        y=input_tensor_2 \
-            if not isinstance(input_tensor_2, np.ndarray) \
-                else tf.convert_to_tensor(input_tensor_2),
-        name=graph_node.name,
+    # Merge two consecutive identical OPs into one
+    # https://github.com/PINTO0309/onnx2tf/issues/230
+    #   A constant is calculated in advance only
+    #   when one of the operations of the current OP
+    #   is a constant and one of the operations of
+    #   the next OP is also a constant.
+    # By merging two OPs into one, an accuracy error always occurs
+    # in the merged OP during the accuracy check.
+    # 1. `Mul` -> `Mul` to `Single-Mul` : `10 * 5 * 8 -> 10 * 40`
+    # 2. `Mul` -> `Div` to `Single-Mul` : `10 * 5 / 8 -> 10 * 0.625`
+    # 3. `Div` -> `Mul` to `Single-Mul` : `10 / 5 * 8 -> 10 * 1.6`
+    # 4. `Div` -> `Div` to `Single-Mul` : `10 / 5 / 8 -> 10 * 0.025`
+    # 5. `Sub` -> `Sub` to `Single-Sub` : `10 - 5 - 8 -> 10 - 13`
+    # 6. `Sub` -> `Add` to `Single-Sub` : `10 - 5 + 8 -> 10 + 3`
+    # 7. `Add` -> `Add` to `Single-Add`  : `10 + 5 + 8 -> 10 + 13`
+    # 8. `Add` -> `Sub` to `Single-Add`  : `10 + 5 - 8 -> 10 - 3`
+    divided_tensor = merge_two_consecutive_identical_ops_into_one(
+        graph_node_input_1=graph_node_input_1,
+        graph_node_input_2=graph_node_input_2,
+        graph_node_output=graph_node_output,
+        before_op_output_shape_trans=before_op_output_shape_trans,
+        input_tensor_1=input_tensor_1,
+        input_tensor_2=input_tensor_2,
+        graph_node=graph_node,
+        tf_layers_dict=tf_layers_dict,
+        tf_func='Div'
     )
+
     ### Partial model
     if kwargs['acc_check'] and tf_partial_model_inputs is not None:
         tf_partial_model_outputs = \
