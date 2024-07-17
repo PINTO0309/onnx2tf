eae37be1de488bde3c50f37e402ba9fc566f5b4c
pinto0309 <rmsdh122@yahoo.co.jp>
2024-07-17 20:05:40 +0900

    Revert "Fixed NHWC flag judgment bug in Transpose of ViT for 3D tensor"
    This reverts commit e2e6e065a2f130c313d24695bcdf39b033541825.

--- onnx2tf/ops/Transpose.py
+++ onnx2tf/ops/Transpose.py
@@ -166,7 +166,7 @@ def make_node(
     elif isinstance(graph_node_input, gs.Variable) \
         and 'nhwc' in tf_layers_dict[graph_node_input.name].keys():
         nhwc = tf_layers_dict[graph_node_input.name]['nhwc']
-        if nhwc and not before_op_output_shape_trans and perm == [i for i in range(len(input_tensor_shape))]:
+        if nhwc and perm == [i for i in range(len(input_tensor_shape))]:
             nhwc = True
         else:
             nhwc = False
